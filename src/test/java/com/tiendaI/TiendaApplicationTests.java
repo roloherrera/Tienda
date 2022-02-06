1f2b3c5bdfab5281cebf4ef0678f2d4f09883d70
PC <PC@DESKTOP-B6P3487>
2022-02-06 16:50:50 -0600

    Commit Prueba

---
+++ src/test/java/com/tiendaI/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
